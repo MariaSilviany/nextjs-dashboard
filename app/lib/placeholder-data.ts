04b9e21fb610f6865e762aa209f3223eda48a4fa
MariaSilviany <231712466@students.uajy.ac.id>
2025-05-15 17:53:41 +0700

    Update placeholder-data.ts

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,27 +1,59 @@
 const cardData = {
   storeRating: 4.7,
-  totalSales: 1300000000,
+  totalSales: 1300000000, // dalam Rupiah
   newCustomers: 432,
   totalProducts: 140,
 };
 
-const revenue = [
+const revenues = [
   {
     product: 'Lilin Aroma Misterius',
     buyer: 'Katharina',
     quantity: 3,
-    status: 'DIKIRIM',
+    status: 'Dikirim',
     total: 198000,
   },
-  ...
+  {
+    product: 'Lilin Aroma Misterius',
+    buyer: 'John',
+    quantity: 1,
+    status: 'Proses',
+    total: 66000,
+  },
+  {
+    product: 'Boneka Seram',
+    buyer: 'Lusia',
+    quantity: 2,
+    status: 'Selesai',
+    total: 132000,
+  },
 ];
 
 const invoices = [
   {
+    product: 'Lilin Aroma Misterius',
+    price: 50000,
+    sold: 2300,
+    imageUrl: '/products/produk1.png',
+  },
+  {
     product: 'Topeng Hantu Horor',
     price: 75000,
-    soldCount: 2300,
-    imageUrl: '/products/topeng-hantu.png',
+    sold: 2300,
+    imageUrl: '/products/produk2.png',
   },
-  ...
+  {
+    product: 'Patung Pemujaan Kuno',
+    price: 150000,
+    sold: 2300,
+    imageUrl: '/products/produk3.png',
+  },
+  {
+    product: 'Boneka Seram',
+    price: 120000,
+    sold: 2300,
+    imageUrl: '/products/produk4.png',
+  },
 ];
+
+export { cardData, revenues, invoices };
