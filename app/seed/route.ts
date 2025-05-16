5fe51a8b26c19774a6becef1c1d0883fcfa3705d
MariaSilviany <231712466@students.uajy.ac.id>
2025-05-16 13:59:15 +0700

    update

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -8,7 +8,7 @@ async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
     CREATE TABLE IF NOT EXISTS users (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,6
       name VARCHAR(255) NOT NULL,
       email TEXT NOT NULL UNIQUE,
       password TEXT NOT NULL
