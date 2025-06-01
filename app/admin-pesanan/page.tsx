e750f0f1661ca898bc530172bbf09b7587fb3549
valeyyyy <valeriafevayosa@gmail.com>
2025-06-01 17:57:16 +0700

    update

--- app/admin-pesanan/page.tsx
+++ app/admin-pesanan/page.tsx
@@ -187,7 +187,7 @@ export default function AdminPesanan1() {
             </a>
           </div>
 
-          <div className="flex items-center px-4 py-3 bg-gray-800 text-white rounded">
+          <div className="flex items-center px-4 py-3 text-gray-300 hover:bg-[#6a2c27] rounded">
             <a href="/admin-produk" className="flex items-center w-full">
               <div className="mr-3">
                 <ProdukIcon />
@@ -205,7 +205,7 @@ export default function AdminPesanan1() {
             </a>
           </div>
 
-          <div className="flex items-center px-4 py-3 text-gray-300 hover:bg-[#6a2c27] rounded">
+          <div className="flex items-center px-4 py-3 bg-gray-800 text-white rounded">
             <a href="admin-pesanan" className="flex items-center w-full">
               <div className="mr-3">
                 <PesananIcon />
