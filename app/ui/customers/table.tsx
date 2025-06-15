f61892aa1060a32dce7ef9cb1614653355dd61cb
MariaSilviany <231712466@students.uajy.ac.id>
2025-06-15 15:17:07 +0700

    up lusitana 2

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
-import { lusitana } from '../fonts';
-import Search from '@/app/ui/search';
+import { lusitana } from '@/ui/fonts';
+import Search from '@/ui/search';
 import {
   CustomersTableType,
   FormattedCustomersTable,
-} from '@/app/lib/definitions';
+} from '@/lib/definitions';
+
 
 export default async function CustomersTable({
   customers,
